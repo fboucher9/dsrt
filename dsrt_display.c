72933320c73be429872b2593d8554d189c507422
Felix Boucher <fboucher9@gmail.com>
2017-02-09 20:17:59 -0500

    added dsrt_display module

---
+++ dsrt_display.c
@@ -0,0 +1,96 @@
+/* See LICENSE for license details. */
+
+/*
+
+Module: dsrt_display.c
+
+Description:
+
+*/
+
+#include "dsrt_os.h"
+
+#include "dsrt_display.h"
+
+#include "dsrt_ctxt.h"
+
+/*
+
+Function: dsrt_display_init
+
+Description:
+
+    Initialize dsrt_display module.
+
+*/
+char
+dsrt_display_init(
+    struct dsrt_ctxt const * const p_ctxt)
+{
+    struct dsrt_display * const p_display = p_ctxt->p_display;
+
+    p_display->dis = XOpenDisplay(NULL);
+
+    if (p_display->dis)
+    {
+        p_display->root = DefaultRootWindow(p_display->dis);
+
+        p_display->screen = DefaultScreen(p_display->dis);
+
+        p_display->depth = DefaultDepth(p_display->dis, p_display->screen);
+
+        p_display->vis = DefaultVisual(p_display->dis, p_display->screen);
+
+        p_display->copyGC = XCreateGC(p_display->dis, p_display->root, 0, NULL);
+
+        p_display->red_mask = p_display->vis->red_mask;
+
+        p_display->green_mask = p_display->vis->green_mask;
+
+        p_display->blue_mask = p_display->vis->blue_mask;
+
+        p_display->rRatio = p_display->red_mask / 255.0;
+
+        p_display->gRatio = p_display->green_mask / 255.0;
+
+        p_display->bRatio = p_display->blue_mask / 255.0;
+
+        return 1;
+    }
+    else
+    {
+#if defined(DSRT_FEATURE_LOG)
+        fprintf(stderr, "unable to open X display\n");
+#endif /* #if defined(DSRT_FEATURE_LOG) */
+
+        return 0;
+    }
+
+} /* dsrt_display_init() */
+
+/*
+
+Function: dsrt_display_cleanup()
+
+Description:
+
+    Free all resources allocated by dsrt_display module.
+
+*/
+void
+dsrt_display_cleanup(
+    struct dsrt_ctxt const * const p_ctxt)
+{
+    struct dsrt_display * const p_display = p_ctxt->p_display;
+
+    if (p_display->dis)
+    {
+        XCloseDisplay(p_display->dis);
+
+        p_display->dis = NULL;
+
+        p_display->vis = NULL;
+    }
+
+} /* dsrt_display_cleanup() */
+
---
+++ dsrt_display.h
@@ -0,0 +1,70 @@
+/* See LICENSE for license details. */
+
+/*
+
+Module: dsrt_display.h
+
+Description:
+
+    Utility functions for X11 display
+
+*/
+
+/* Reverse include guard */
+#if defined(INC_DSRT_DISPLAY_H)
+#error include dsrt_display.h once
+#endif /* #if defined(INC_DSRT_DISPLAY_H) */
+
+#define INC_DSRT_DISPLAY_H
+
+/* Predefine context handle */
+struct dsrt_ctxt;
+
+/*
+
+Structure: dsrt_display
+
+Description:
+
+    Storage of useful resources related to X11 display.
+
+*/
+struct dsrt_display
+{
+    Display * dis;
+
+    Visual * vis;
+
+    GC copyGC;
+
+    Window root;
+
+    unsigned long int red_mask;
+
+    unsigned long int green_mask;
+
+    unsigned long int blue_mask;
+
+    double rRatio;
+
+    double gRatio;
+
+    double bRatio;
+
+    int screen;
+
+    int depth;
+
+}; /* struct dsrt_display */
+
+/* --- Interface --- */
+
+char
+dsrt_display_init(
+    struct dsrt_ctxt const * const p_ctxt);
+
+void
+dsrt_display_cleanup(
+    struct dsrt_ctxt const * const p_ctxt);
+
+/* end-of-file: dsrt_display.h */
